174182d12800815975f4d102e86ab51aebaf46ae
Talha Razzaq <tctnpk@gmail.com>
2023-03-03 00:49:06 +0500

    just started

---
+++ src/components/NameSection.js
@@ -0,0 +1,9 @@
+export default function  NameSection() {
+    return (
+        <div className="nameSection">
+            <h1>Talha Razzaq</h1>
+            <div>Front-end Developer</div>
+            <div className="emailDiv">trazzaq9@gmail.com</div>
+        </div>
+    )
+}
